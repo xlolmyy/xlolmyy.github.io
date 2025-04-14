7bc03d0971e13442320eb4c30dbabe2c93e1649e
xlolmyy <79284019+xlolmyy@users.noreply.github.com>
2025-04-14 15:01:54 +0200

    test

---
+++ place/test.js
@@ -0,0 +1,38 @@
+// Suche
+document.addEventListener("DOMContentLoaded", function () {
+    const searchInput = document.getElementById("searchInput");
+    const projects = document.querySelectorAll(".project-card");
+
+    searchInput.addEventListener("input", function () {
+        const term = this.value.toLowerCase();
+
+        
+        projects.forEach((project) => {
+            const text = project.innerText.toLowerCase();
+            if (text.includes(term)) {
+                project.style.display = "block";
+                project.classList.add("highlight");
+            } else {
+                project.style.display = "none";
+                project.classList.remove("highlight");
+            }
+
+            if (term === "") {
+                project.classList.remove("highlight");
+                project.style.display = "block";
+            }
+        });
+    });
+
+    // Lightbox
+    document.querySelectorAll(".project-card img").forEach(img => {
+        img.addEventListener("click", () => {
+            document.getElementById("lightbox-img").src = img.src;
+            document.getElementById("lightbox").style.display = "flex";
+        });
+    });
+
+    document.getElementById("lightbox-close").addEventListener("click", () => {
+        document.getElementById("lightbox").style.display = "none";
+    });
+});
